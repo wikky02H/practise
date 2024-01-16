47e45d03ba9e0b9d73d86ed9ab873f3bcce04a23
wikky02H <109713162+wikky02H@users.noreply.github.com>
2024-01-16 19:59:29 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import { toaster } from "react-hot-toast";
+import RootRouter from "@/router/Root";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ConfirmDialog } from "primereact/confirmdialog";
